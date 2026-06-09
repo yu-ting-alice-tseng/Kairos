14e2c4dcf0e624df7e88aa51dc17d4f767507e6f
AliceTseng0810 <yexiu07060810@gmail.com>
2026-06-09 18:10:48 +0200

    Auto-update: File changed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -26,8 +26,3 @@ export const prisma: PrismaClient =
   globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export const prisma: PrismaClient =
-  globalForPrisma.prisma ?? createPrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
